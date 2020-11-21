f8491c60498f302784d2c3f7862c1e19818f1b2d
Julian Goh <juliangohsl@gmail.com>
2020-11-21 22:54:09 +0800

    feat: Update client script
    Updated client script to make it fit the format of the server script.
    This means it will print out the usage and you can configure the
    SERVER_PORT to connect on
    
    Contributes to: #10
    
    Signed-off-by: Julian Goh <juliangohsl@gmail.com>

--- run-client.sh
+++ run-client.sh
@@ -7,8 +7,16 @@ CLIENT_IMAGE="${PROJECT_NAME}-client"
 IMAGE_TAG="latest"
 CONTAINER_REPORT_PATH="/resources/report"
 REPORT_FOLDER="${WORKDIR}/generated/report"
+DEFAULT_SERVER_PORT="8888"
+
+echo "Usage: use this script to run a client after running ./build.sh to build the necessary docker image"
+echo "    Arg 1: the port of the server you want to connect to  (default: ${DEFAULT_SERVER_PORT})"
+
+SERVER_PORT=${1:-${DEFAULT_SERVER_PORT}}
+echo "Client will connect to server on port ${SERVER_PORT}"
 
 docker run -it \
     --network="host" \
     -v "$REPORT_FOLDER":${CONTAINER_REPORT_PATH} \
+    -e SERVER_PORT="${SERVER_PORT}" \
     "${CLIENT_IMAGE}:${IMAGE_TAG}"
